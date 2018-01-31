43db9805e48e8e5e64374b4b06b905610d49922c
wananmingithub <wananmindlut@gmail.com>
2018-01-31 09:18:52 +0800

    second commit

---
+++ FaceExpression/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace FaceExpression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(396, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PhotoChoose";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(633, 20);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Analyse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Font = new System.Drawing.Font("Showcard Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(508, 123);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 41);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(396, 223);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(131, 24);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(630, 223);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(129, 24);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(396, 269);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(131, 24);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(630, 269);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(129, 24);
+            this.textBox6.TabIndex = 8;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 22);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(339, 339);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(396, 308);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 53);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Camera";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Showcard Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(630, 308);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 53);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "FaceGet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(788, 400);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Showcard Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "人脸表情识别系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ FaceExpression/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.Cuda;
+using System.Diagnostics;
+using Emgu.CV.UI;
+using System.Security.Permissions;
+
+namespace FaceExpression
+{
+    public partial class Form1 : Form
+    {
+        CascadeClassifier haar;
+        private Image myImage = null;
+        Image<Bgr, Byte> frame = null;     //原始读入图像     
+        Image<Gray, Byte> gray = null;     //灰度图像
+        Image<Bgr, Byte> smallframe = null;     //裁剪后图像
+        double scale = 1.5;
+
+        private VideoCapture capture;
+        private bool capture_flag = true;
+        private System.Timers.Timer capture_tick;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            haar = new CascadeClassifier("haarcascade_frontalface_alt.xml");     //载入人脸级联
+            capture_tick = new System.Timers.Timer();
+            capture_tick.Interval = 100;
+            capture_tick.Enabled = Enabled;
+            capture_tick.Stop();
+            capture_tick.Elapsed += new System.Timers.ElapsedEventHandler(CaptureProcess);
+
+            textBox2.Visible = false;
+           
+        }
+        //选择图片并调用后台进行分析
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Visible = false;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "选择文件";
+            ofd.Filter = "*.jpg,*.jpeg,*.bmp,*.gif,*.ico,*.png,*.tif,*.wmf|*.jpg;*.jpeg;*.bmp;*.gif;*.ico;*.png;*.tif;*.wmf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //textBox1.Text = ofd.FileName;
+                myImage = System.Drawing.Image.FromFile(ofd.FileName);
+                frame = new Image<Bgr, byte>(ofd.FileName);
+                //pictureBox1.Image = myImage;
+                smallframe = frame.Resize(1 / scale, Emgu.CV.CvEnum.Inter.Linear);//缩放摄像头拍到的大尺寸照片 
+                
+                if (smallframe.NumberOfChannels == 3)
+                {
+                    gray = smallframe.Convert<Gray, Byte>(); //Convert it to Grayscale
+                    
+                }
+                gray._EqualizeHist();//均衡化 
+                
+                
+                Rectangle[] rects = haar.DetectMultiScale(gray, 1.3, 3, new Size(30, 30), Size.Empty);
+                Image<Bgr, Byte> temp = null;
+                foreach (Rectangle r in rects)
+                {
+                    //This will focus in on the face from the haar results its not perfect but it will remove a majoriy  
+                    //of the background noise  
+                    Rectangle facesDetected = r;
+                    facesDetected.X += (int)(facesDetected.Height * 0.2);
+                    facesDetected.Y += (int)(facesDetected.Width * 0.4);
+                    facesDetected.Height += (int)(facesDetected.Height * 0.5);
+                    facesDetected.Width += (int)(facesDetected.Width * 0.3);
+                    temp = frame.GetSubRect(facesDetected);
+
+                    frame.Draw(facesDetected, new Bgr(Color.Red), 3);//绘制检测框  
+                }
+                imageBox1.Image = temp;
+                string fileName = "img" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+                temp.Save("./image/" + fileName);
+
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = "classification/classification.exe";
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.Arguments = "classification/rgb.prototxt classification/rgb_mean.caffemodel classification/rgb_mean.binaryproto classification/rgb.txt "+ "./image/"+ fileName;
+                p.Start();
+                p.WaitForExit();
+            }
+
+          }
+        //分析结果从txt文件中读取输出在界面中
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("test.txt");
+            string nextline = sr.ReadLine();
+            //string temp = nextline.Substring(10, 8);
+            textBox2.Visible = true;
+            if (nextline.Contains("angry"))
+            {
+                textBox2.Text = "ANGRY";
+                //pictureBox1.Image = Image.FromFile("emotion/0.jpg");
+            }
+            else if (nextline.Contains("disgust"))
+            {
+                textBox2.Text = "DISGUST";
+                //pictureBox1.Image = Image.FromFile("emotion/1.jpg");
+            }
+            else if (nextline.Contains("fear"))
+            {
+                textBox2.Text = "NEUTRAL";
+                //pictureBox1.Image = Image.FromFile("emotion/2.jpg");
+            }
+            else if (nextline.Contains("happy"))
+            {
+                textBox2.Text = "HAPPY";
+                //pictureBox1.Image = Image.FromFile("emotion/3.jpg");
+            }
+            else if (nextline.Contains("neutral"))
+            {
+                textBox2.Text = "NEUTRAL";
+                //pictureBox1.Image = Image.FromFile("emotion/4.jpg");
+            }
+            else if (nextline.Contains("sad"))
+            {
+                textBox2.Text = "SAD";
+                //pictureBox1.Image = Image.FromFile("emotion/5.jpg");
+            }
+            else if (nextline.Contains("surprise"))
+            {
+                textBox2.Text = "SURPRISE";
+            }
+            else
+            {
+            }
+            //textBox2.Text = temp;
+            nextline = sr.ReadLine();
+            textBox3.Text = nextline;
+            nextline = sr.ReadLine();
+            textBox4.Text = nextline;
+            nextline = sr.ReadLine();
+            textBox5.Text = nextline;
+            nextline = sr.ReadLine();
+            textBox6.Text = nextline;
+            sr.Close();
+         }
+        private void CaptureProcess(object sender, EventArgs arg)
+        {
+            Mat temframe = capture.QueryFrame();
+            frame = temframe.ToImage<Bgr, Byte>();
+            smallframe = frame.Resize(1 / scale, Emgu.CV.CvEnum.Inter.Linear);//缩放摄像头拍到的大尺寸照片  
+            if (smallframe.NumberOfChannels == 3)
+            {
+                gray = smallframe.Convert<Gray, Byte>(); //Convert it to Grayscale  
+            }
+            gray._EqualizeHist();//均衡化  
+            Rectangle[] rects = haar.DetectMultiScale(gray, 1.3, 3, new Size(30, 30), Size.Empty);
+            foreach (Rectangle r in rects)
+            {
+                //This will focus in on the face from the haar results its not perfect but it will remove a majoriy  
+                //of the background noise  
+                Rectangle facesDetected = r;
+                facesDetected.X += (int)(facesDetected.Height * 0.4);
+                facesDetected.Y += (int)(facesDetected.Width * 0.4);
+                facesDetected.Height += (int)(facesDetected.Height * 0.5);
+                facesDetected.Width += (int)(facesDetected.Width * 0.3);
+
+                frame.Draw(facesDetected, new Bgr(Color.Red), 3);//绘制检测框  
+            }
+            imageBox1.Image = frame;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new VideoCapture();
+                }
+                catch (NullReferenceException except)
+                {
+                    MessageBox.Show(except.Message);
+                }
+            }
+
+            if (capture != null)
+            {
+                if (capture_flag)
+                {
+                    //Application.Idle += new EventHandler(CaptureProcess);  
+                    capture_tick.Start();
+                    button3.Text = "Stop";
+                }
+                else
+                {
+                    //Application.Idle -= new EventHandler(CaptureProcess);  
+                    capture_tick.Stop();
+                    button3.Text = "Start";
+                }
+                capture_flag = !capture_flag;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Mat temframe = capture.QueryFrame();
+            frame = temframe.ToImage<Bgr, Byte>();
+            smallframe = frame.Resize(1 / scale, Emgu.CV.CvEnum.Inter.Linear);//缩放摄像头拍到的大尺寸照片  
+            if (smallframe.NumberOfChannels == 3)
+            {
+                gray = smallframe.Convert<Gray, Byte>(); //Convert it to Grayscale  
+            }
+            gray._EqualizeHist();//均衡化  
+            Rectangle[] rects = haar.DetectMultiScale(gray, 1.3, 3, new Size(30, 30), Size.Empty);
+            foreach (Rectangle r in rects)
+            {
+                //This will focus in on the face from the haar results its not perfect but it will remove a majoriy  
+                //of the background noise  
+                Rectangle facesDetected = r;
+                facesDetected.X += (int)(facesDetected.Height * 0.6);
+                facesDetected.Y += (int)(facesDetected.Width * 0.4);
+                facesDetected.Height += (int)(facesDetected.Height * 0.5);
+                facesDetected.Width += (int)(facesDetected.Width * 0.3);
+
+                frame.Draw(facesDetected, new Bgr(Color.Red), 3);//绘制检测框  
+            }
+            imageBox1.Image = frame;
+            //实时拍照照片保存
+            string fileName = "img" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+            frame.Save("./image/"+ fileName);
+
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "classification/classification.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = "classification/rgb.prototxt classification/rgb_mean.caffemodel classification/rgb_mean.binaryproto classification/rgb.txt " +"./image/"+fileName;
+            p.Start();
+            p.WaitForExit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
